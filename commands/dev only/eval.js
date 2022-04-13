bc8d946cb5e2607c5fc3ab79fa4e7a983c3e82dc
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-13 08:24:23 +0200

    accident

--- commands/dev only/eval.js
+++ commands/dev only/eval.js
@@ -2,5 +2,5 @@ module.exports ={
 name:"eval",
 code: `
 $eval[$message]
-$onlyForIDs[$botOwnerID;only $username[$botOwnerID can execute this command]
+$onlyForIDs[$botOwnerID;only $username[$botOwnerID] can execute this command]
 `} // allows excuting a code for testing purposes
