e7c5468043a47233a454e818c877185f323b2840
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-04-13 08:30:14 +0200

    fix grammar 💀

--- commands/dev only/Djseval.js
+++ commands/dev only/Djseval.js
@@ -3,4 +3,4 @@ name:"djseval",
 code: `
 $djsEval[$message]
 $onlyForIDs[$botOwnerID;only $username[$botOwnerID] can execute this command]
-`} // allows excuting a code for testing purposes, but with discord.ja given code
+`} // allows excuting a code for testing purposes, but with discord.js given code
